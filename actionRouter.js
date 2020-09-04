90aff1217b25266e73b03efa9290cf5d48a53907
DeeDowns <dee_downs@outlook.com>
2020-09-04 13:26:56 -0400

    created validateActionId middleware; GET all actions and action by id completed

--- actionRouter.js
+++ actionRouter.js
@@ -1,20 +1,62 @@
 const express = require('express')
 
 const Actions = require('./data/helpers/actionModel')
+const { json } = require('express')
 
 const router = express.Router()
 
-router.get('/:id', (req, res) => {
-    const { id } = req.params
+//GET all actions
+router.get('/', (req, res) => {
+    
 
-    Actions.get(id)
+    Actions.get()
     .then(actions => {
         console.log(actions)
+        res.status(200).json(actions)
     })
     .catch(error => {
         console.log(error)
+        res.status(500).json({message: 'error fetching actions' })
     })
 })
+
+//GET action by id
+router.get('/:id', validateActionId, (req, res) => {
+    res.status(200).json(req.action)
+   
+   
+//     const { id } = req.params
+
+//     Actions.get(id) 
+//     .then(action => {
+//         console.log(action)
+//         res.status(200).json(action)
+//     })
+//     .catch(error => {
+//         console.log(error)
+//         res.status(500).json({message: 'error fetching action' })
+//     })
+})
+
+
+/******* Middleware *******/
+function validateActionId(req, res, next) {
+    const { id } = req.params
+
+    Actions.get(id) 
+    .then(action => {
+        if(action) {
+            req.action = action
+            next()
+        } else {
+            res.status(404).json({ message: 'action not found' })
+        }
+    })
+    .catch(error => {
+        res.status(500).json({message: 'error fetching action' })
+    })
+
+}
 
 
 module.exports = router
